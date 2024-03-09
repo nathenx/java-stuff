5a75717ae0ff978f67287328dd57441196db5b25
nathenx <nathenyamamoto@gmail.com>
2024-03-09 13:30:54 -0500

    hello

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello {
+public static void main(String[] args) {
+System.out.println("Hello Tim");
+
+boolean isAlien = false;
+if (isAlien == false)
+    System.out.println("It is not an alien!");
+    System.out.println("And I am scared of aliens");
+
+}
+}
