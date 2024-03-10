84d5683ab4fb28498afe344a0e5040c439c7318d
nathenx <nathenyamamoto@gmail.com>
2024-03-10 11:16:55 -0400

    adding more stuff hellow

--- hello.java
+++ hello.java
@@ -7,5 +7,9 @@ if (isAlien == false)
     System.out.println("It is not an alien!");
     System.out.println("And I am scared of aliens");
 
+    int topScore = 100;
+    if (topScore == 100)
+        System.out.println("You got the high score!");
+
 }
 }
